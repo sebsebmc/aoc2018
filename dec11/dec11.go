9243379d2a64b82ab94a48d019b8e6b63ea93c75
Sebastian Chlopecki <sebscoolmail@gmail.com>
2018-12-11 22:38:34 -0600

    Day 11

--- dec11/dec11.go
+++ dec11/dec11.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"math"
@@ -13,7 +14,74 @@ func main() {
 }
 
 func part1() {
-	lines := readFileLines()
+	input := 8868
+	grid := make([][]int, 300)
+	for i := 0; i < 300; i++ {
+		grid[i] = make([]int, 300)
+		for j := 0; j < 300; j++ {
+			rackId := (j + 1) + 10
+			powerLevel := rackId * (i + 1)
+			powerLevel += input
+			powerLevel *= rackId
+			powerLevel = (powerLevel % 1000) / 100
+			powerLevel -= 5
+			grid[i][j] = powerLevel
+		}
+	}
+	sums := make([]int, 300)
+	s := 3
+	fmt.Println(s)
+	for i := 0; i < 300-s; i++ {
+		for j := 0; j < 300-s; j++ {
+			for x := 0; x < s; x++ {
+				for y := 0; y < s; y++ {
+					sums[i*300+j] += grid[i+x][j+y]
+				}
+			}
+		}
+	}
+	max, idx := findMax(sums)
+	fmt.Println(max, idx, (idx%300)+1, (idx/300)+1)
+}
+
+func part2() {
+	input := 8868
+	grid := make([][]int, 300)
+	for i := 0; i < 300; i++ {
+		grid[i] = make([]int, 300)
+		for j := 0; j < 300; j++ {
+			rackId := (j + 1) + 10
+			powerLevel := rackId * (i + 1)
+			powerLevel += input
+			powerLevel *= rackId
+			powerLevel = (powerLevel % 1000) / 100
+			powerLevel -= 5
+			grid[i][j] = powerLevel
+		}
+	}
+	sums := make([][]int, 300)
+
+	for s := 1; s < 300; s++ {
+		sums[s] = make([]int, 300*300)
+		fmt.Println(s)
+		for i := 0; i < 300-s; i++ {
+			for j := 0; j < 300-s; j++ {
+				for x := 0; x < s; x++ {
+					for y := 0; y < s; y++ {
+						sums[s][i*300+j] += grid[i+x][j+y]
+					}
+				}
+			}
+		}
+	}
+	maxes, idxs := make([]int, 300), make([]int, 300)
+	for i := 0; i < len(sums); i++ {
+		maxes[i], idxs[i] = findMax(sums[i])
+	}
+	max, idx := findMax(maxes)
+	fmt.Println(idx)
+	idx = idxs[idx]
+	fmt.Println(max, idx, (idx%300)+1, (idx/300)+1)
 }
 
 func findMin(nums []int) (int, int) {
@@ -30,7 +98,7 @@ func findMin(nums []int) (int, int) {
 func findMax(nums []int) (int, int) {
 	max, idx := math.MinInt32, -1
 	for i := 0; i < len(nums); i++ {
-		if nums[i] < max {
+		if nums[i] > max {
 			max = nums[i]
 			idx = i
 		}
--- dec6/dec6.go
+++ dec6/dec6.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"strings"
 )
 
@@ -16,6 +17,28 @@ func part1() {
 
 func part2() {
 
+}
+
+func findMin(nums []int) (int, int) {
+	min, idx := math.MaxInt32, -1
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < min {
+			min = nums[i]
+			idx = i
+		}
+	}
+	return min, idx
+}
+
+func findMax(nums []int) (int, int) {
+	max, idx := math.MinInt32, -1
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > max {
+			max = nums[i]
+			idx = i
+		}
+	}
+	return max, idx
 }
 
 func readFileLines() []string {
